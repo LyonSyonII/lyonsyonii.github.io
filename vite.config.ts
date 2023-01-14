8b7a34078fa2051243d965bc5a15dbcac76bdc17
LyonSyonII <slimystudios@protonmail.com>
2023-01-14 14:55:55 +0100

    Added ts migrate

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,6 @@ function getTailwindConfig() {
 }
 
 export default defineConfig({
-  // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
-  // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
   base: "/",
   plugins: [preact(), visualizer()],
   resolve: {
